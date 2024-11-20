69f987d530df90f16718b709a44ff7f52c9715e4
ussiel <ussiel@gmail.com>
2024-11-19 23:03:31 -0600

    ajustes

--- modulos/franky/configure/blocks.php
+++ modulos/franky/configure/blocks.php
@@ -5,12 +5,12 @@ return json_encode([
         'reference' => '._search_bar',
         'html' => getCatalogBuscadorPrincipal()
     ], 
-    [
+   /* [
         'position' => 'after',
         'reference' => '.cont_mi_carrito',
         'html' => getHTMLRenderMinicart()
         
-    ], 
+    ], */
     [
         'position' => 'before',
         'reference' => '._category_menu',
